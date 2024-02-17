287e0cfff94ff01cec93cfc603ebe43d2772f7e3
yosuanetzme18 <Ganasa18@yahoo.com>
2024-02-17 16:58:17 +0700

    update: table crud

--- main.go
+++ main.go
@@ -86,8 +86,8 @@ func createTable(model Model) {
 	// Create a dynamic struct
 	fields := []reflect.StructField{
 		{Name: "Id", Type: reflect.TypeOf(0), Tag: reflect.StructTag(`gorm:"primaryKey"`)},
-		{Name: "CreatedAt", Type: reflect.TypeOf(time.Time{}), Tag: reflect.StructTag(`gorm:"default:CURRENT_TIMESTAMP()"`)},
-		{Name: "UpdatedAt", Type: reflect.TypeOf(time.Time{}), Tag: reflect.StructTag(`gorm:"default:CURRENT_TIMESTAMP()"`)},
+		{Name: "CreatedAt", Type: reflect.TypeOf(time.Time{}), Tag: reflect.StructTag(`gorm:"default:current_timestamp"`)},
+		{Name: "UpdatedAt", Type: reflect.TypeOf(time.Time{}), Tag: reflect.StructTag(`gorm:"default:current_timestamp"`)},
 		{Name: "DeletedAt", Type: reflect.TypeOf(gorm.DeletedAt{})},
 	}
 	for _, field := range model.Fields {
@@ -122,6 +122,48 @@ func createTable(model Model) {
 	db.Table(tableName).AutoMigrate(instance)
 
 }
+
+// Handlers Model
+
+func createModel(c *gin.Context) {
+	var newModel Model
+	if err := c.BindJSON(&newModel); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Create the model
+	if err := db.Create(&newModel).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	createTable(newModel)
+	c.JSON(http.StatusOK, newModel)
+}
+
+func listModels(c *gin.Context) {
+	var models []Model
+	if err := db.Preload(clause.Associations).Find(&models).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, models)
+}
+
+func getModel(c *gin.Context) {
+	id := c.Param("id")
+	var model Model
+	if err := db.Preload(clause.Associations).First(&model, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Model not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, model)
+}
+
+// Handler Table
 
 func listRecords(c *gin.Context) {
 	tableName := c.Param("table")
@@ -201,62 +243,143 @@ func createRecord(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": requestData})
 }
 
+func getRecord(c *gin.Context) {
+	tableName := c.Param("table")
+	idRecord := c.Param("id")
+
+	// Define a dynamic struct to hold the record data
+	record := make(map[string]interface{})
+
+	// Query the database to retrieve the record
+	query := fmt.Sprintf("SELECT * FROM %s WHERE id = ?", tableName)
+	db.Raw(query, idRecord).Scan(&record)
+
+	// Check if the record exists
+	if len(record) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Record not found"})
+		return
+	}
+
+	// Return the record data
+	c.JSON(http.StatusOK, gin.H{"data": record})
+}
+
+func deleteRecord(c *gin.Context) {
+	tableName := c.Param("table")
+	idRecord := c.Param("id")
+
+	// Check if the record exists before attempting to delete
+	var count int64
+	if err := db.Table(tableName).Where("id = ?", idRecord).Count(&count).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if count == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Record not found"})
+		return
+	}
+
+	// Perform the delete operation
+	if err := db.Table(tableName).Where("id = ?", idRecord).Delete(nil).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Return success message
+	c.JSON(http.StatusOK, gin.H{"data": "success delete"})
+}
+
+func softDeleteRecord(c *gin.Context) {
+	tableName := c.Param("table")
+	idRecord := c.Param("id")
+
+	// Check if the record exists before attempting to soft delete
+	var count int64
+	if err := db.Table(tableName).Where("id = ?", idRecord).Count(&count).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if count == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Record not found"})
+		return
+	}
+
+	// Perform the soft delete operation
+	if err := db.Table(tableName).Where("id = ?", idRecord).Updates(map[string]interface{}{"deleted_at": time.Now()}).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Return success message
+	c.JSON(http.StatusOK, gin.H{"data": "success soft delete"})
+}
+
+func updateRecord(c *gin.Context) {
+	tableName := c.Param("table")
+	idRecord := c.Param("id")
+
+	// Check if the record exists before attempting to update
+	var count int64
+	if err := db.Table(tableName).Where("id = ?", idRecord).Count(&count).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if count == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Record not found"})
+		return
+	}
+
+	// Decode the JSON request body into a map
+	var requestData map[string]interface{}
+	decoder := json.NewDecoder(c.Request.Body)
+	if err := decoder.Decode(&requestData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Set the 'updated_at' field to the current timestamp
+	requestData["updated_at"] = time.Now()
+
+	// Perform the update operation
+	if err := db.Table(tableName).Where("id = ?", idRecord).Updates(requestData).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Fetch the updated record from the database as a map
+	var updatedRecord map[string]interface{}
+	if err := db.Table(tableName).Where("id = ?", idRecord).Scan(&updatedRecord).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Return the updated record as a map
+	c.JSON(http.StatusOK, gin.H{"data": updatedRecord})
+}
+
 func main() {
 
 	r := gin.Default()
 
-	// Define API routes
+	// Define API routes Model
 	r.POST("/models", createModel)
 	r.GET("/models", listModels)
 	r.GET("/models/:id", getModel)
 
-	// Define Table
+	// Define API routes Table
 	r.GET("/tables/:table", listRecords)
 	r.POST("/tables/:table", createRecord)
+	r.GET("/tables/:table/:id", getRecord)
+	r.PATCH("/tables/:table/:id", updateRecord)
+	r.DELETE("/tables/:table/:id", deleteRecord)
+	r.DELETE("/tables-soft/:table/:id", softDeleteRecord)
 
 	// Run the server
 	err := r.Run(":8080")
 	if err != nil {
 		panic(err)
 	}
-}
-
-// Handlers
-
-func createModel(c *gin.Context) {
-	var newModel Model
-	if err := c.BindJSON(&newModel); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Create the model
-	if err := db.Create(&newModel).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	createTable(newModel)
-	c.JSON(http.StatusOK, newModel)
-}
-
-func listModels(c *gin.Context) {
-	var models []Model
-	if err := db.Preload(clause.Associations).Find(&models).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, models)
-}
-
-func getModel(c *gin.Context) {
-	id := c.Param("id")
-	var model Model
-	if err := db.Preload(clause.Associations).First(&model, id).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Model not found"})
-		return
-	}
-
-	c.JSON(http.StatusOK, model)
 }
